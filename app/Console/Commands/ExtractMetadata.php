4e5d77b21e173749bdcfcef95831dfa5a403ed26
Joey J <joey@wyxos.com>
2025-06-28 14:15:17 +0400

    feat: add audio metadata extraction functionality and related scripts

--- app/Console/Commands/ExtractMetadata.php
+++ app/Console/Commands/ExtractMetadata.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\File;
+use FFMpeg\FFMpeg;
 use Illuminate\Console\Command;
 
 class ExtractMetadata extends Command
@@ -26,14 +27,43 @@ class ExtractMetadata extends Command
      */
     public function handle()
     {
+        File::where('mime_type', 'audio/mpeg')->chunk(100, function ($files) {
+            foreach ($files as $file) {
+                // execute node script /scripts/extract-metadata.js with file path as argument
+                $this->info("Extracting metadata for file: {$file->path}");
+
+                $output = shell_exec("node scripts/extract-metadata.js \"{$file->path}\"");
+
+                dd(json_decode($output, true));
+
+                Storage::put("metadata/{$file->id}.json", $output);
+
+                if ($output) {
+                    $this->info("Metadata extracted successfully: {$output}");
+                } else {
+                    $this->error("Failed to extract metadata for file: {$file->path}");
+                }
+            }
+        });
+
+
         // file where audio
         $file = File::where('mime_type', 'audio/mpeg')
             ->first();
 
-        if (!$file) {
+        if ($file) {
+            // execute node script /scripts/extract-metadata.js with file path as argument
+            $this->info("Extracting metadata for file: {$file->path}");
 
+            $output = shell_exec("node scripts/extract-metadata.js \"{$file->path}\"");
+
+            dd(json_decode($output, true));
+
+            if ($output) {
+                $this->info("Metadata extracted successfully: {$output}");
+            } else {
+                $this->error("Failed to extract metadata for file: {$file->path}");
+            }
         }
-
-        dd($file->path);
     }
 }
---
+++ scripts/extract-metadata.js
@@ -0,0 +1,13 @@
+import { parseFile } from 'music-metadata';
+
+(async () => {
+    try {
+        const filePath = process.argv[2];
+        const metadata = await parseFile(filePath);
+
+        console.log(JSON.stringify(metadata, null, 2)); // pure JSON output
+    } catch (error) {
+        console.error('Error parsing metadata:', error.message);
+        process.exit(1);
+    }
+})();
