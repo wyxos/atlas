dd60f73a2f14401b736a84545cd9775afdad9a31
Joey J <joey@wyxos.com>
2025-06-28 08:48:16 +0400

    feat: add FileFactory and FileSearchTest for improved file model testing

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Scout\Searchable;
 
 class File extends Model
 {
-    use Searchable;
+    use HasFactory, Searchable;
     /**
      * The attributes that are mass assignable.
      *
