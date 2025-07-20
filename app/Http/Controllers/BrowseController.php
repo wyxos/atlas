c1bff396c01918592527b75ffae9818dce60095e
Joey J <joey@wyxos.com>
2025-07-21 03:49:15 +0400

    refactor: update blacklistImage function to use axios for backend calls

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\File;
 use App\Services\CivitAIService;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -24,7 +26,7 @@ class BrowseController extends Controller
     /**
      * Blacklist a file.
      */
-    public function blacklist(Request $request, File $file): \Illuminate\Http\RedirectResponse
+    public function blacklist(Request $request, File $file): \Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
     {
         $request->validate([
             'reason' => 'nullable|string|max:255'
@@ -34,6 +36,14 @@ class BrowseController extends Controller
             'is_blacklisted' => true,
             'blacklist_reason' => $request->input('reason')
         ]);
+
+        // Return JSON response for AJAX requests, redirect for regular requests
+        if ($request->expectsJson()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Item has been blacklisted'
+            ]);
+        }
 
         return back()
             ->with('message', 'Item has been blacklisted');
