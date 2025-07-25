45565bd758b94ecc52c065c8cd0dc79ba990dfa8
Joey J <joey@wyxos.com>
2025-07-26 01:47:22 +0400

    Update Horizon to allow admin access and fix WebSocket configuration

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,9 +28,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+            return optional($user)->is_admin === true;
         });
     }
 }
