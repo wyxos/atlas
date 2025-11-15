de01247fdb987251b578632700650f4090e9dfd7
Wyxos <wyxos@proton.me>
2025-11-15 22:43:20 +0400

    chore: remove queue worker helper scripts

--- start-queues.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting 3 queue workers...
-
-start "Queue Worker 1" php artisan queue:work
-start "Queue Worker 2" php artisan queue:work
-start "Queue Worker 3" php artisan queue:work
-
-echo Started 3 queue workers successfully!
