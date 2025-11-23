87736a86ca1c0c303f3b3b736ce440f0fd28abcb
Wyxos <wyxos@proton.me>
2025-11-23 22:54:22 +0400

    refactor: extract SVG to component and empty README

---
+++ resources/views/partials/favicons.blade.php
@@ -0,0 +1,13 @@
+<link rel="icon" type="image/svg+xml" href="/favicon.svg?v=2">
+<link rel="manifest" href="/manifest.webmanifest">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="theme-color" content="#ffffff">
+<meta name="application-name" content="Atlas">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+<meta name="apple-mobile-web-app-title" content="Atlas">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="msapplication-config" content="/browserconfig.xml">
+
+
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,3 +5,5 @@ test('the application returns a successful response', function () {
 
     $response->assertStatus(200);
 });
+
+
