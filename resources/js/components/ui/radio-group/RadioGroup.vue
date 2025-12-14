0efec28d89a1d3626285f83c85aec56e2d796796
Wyxos <wyxos@proton.me>
2025-12-14 05:29:22 +0400

    feat: add `RadioGroup` and `RadioGroupItem` components and integrate advanced filter controls into BrowseTabContent

---
+++ resources/js/components/ui/radio-group/RadioGroup.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { RadioGroupRootEmits, RadioGroupRootProps } from "reka-ui"
+import { RadioGroupRoot, useForwardPropsEmits } from "reka-ui"
+
+const props = defineProps<RadioGroupRootProps>()
+const emits = defineEmits<RadioGroupRootEmits>()
+
+const forwarded = useForwardPropsEmits(props, emits)
+</script>
+
+<template>
+  <RadioGroupRoot
+    data-slot="radio-group"
+    v-bind="forwarded"
+  >
+    <slot />
+  </RadioGroupRoot>
+</template>
+
