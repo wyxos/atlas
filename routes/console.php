262c9a878eff908343dfb86d6b3e67900fc4bc0e
Wyxos <wyxos@proton.me>
2025-12-28 03:10:28 +0400

    chore: add Laravel Horizon integration and configuration

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Horizon maintenance tasks
+Schedule::command('horizon:snapshot')->everyFiveMinutes();
+Schedule::command('horizon:clear-metrics')->hourly();
+Schedule::command('horizon:clear')->daily();
