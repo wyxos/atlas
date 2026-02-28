29ad6db3df4fbb2b22eb04891f7051a4e7ce9115
Wyxos <github@wyxos.com>
2026-02-28 08:49:31 +0400

    Exclude extension API routes from CSRF middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/extension/*',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         Integration::handles($exceptions);
